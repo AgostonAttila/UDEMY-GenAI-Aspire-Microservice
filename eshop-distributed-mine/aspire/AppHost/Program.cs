2b0c6fbc9cbefb463f71d78ecde421638233e475
AgostonAttila <atesz014@gmail.com>
2025-03-31 17:53:14 +0200

    add Catalog API

--- eshop-distributed-mine/aspire/AppHost/Program.cs
+++ eshop-distributed-mine/aspire/AppHost/Program.cs
@@ -1,3 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Backing Services
+var postgres = builder
+        .AddPostgres("postgres")
+        .WithPgAdmin()
+        //.WithDataVolume()
+        .WithLifetime(ContainerLifetime.Persistent);
+
+var catalogDb = postgres.AddDatabase("catalogdb");
+
+builder.AddProject<Projects.Catalog>("catalog")
+    .WithReference(catalogDb)
+    .WaitFor(catalogDb);
+
 builder.Build().Run();
